385e478c44f4ad838277aabb7bd11f1435ed0cde
donzyC <donzy8787@gmail.com>
2024-12-20 23:57:29 -0800

    Fix mobile responsiveness and form label animation

--- main.py
+++ main.py
@@ -128,5 +128,5 @@ def developer():
 
 # python main
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5002))
     app.run(host='0.0.0.0', port=port)
